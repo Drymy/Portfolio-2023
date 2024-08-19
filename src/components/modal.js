a0bc9009596f50fa4f42e0374b8ddc50f342bfa0
Drymy <simicstef34@gmail.com>
2024-08-19 20:07:56 +0200

    fixed moog photo

--- src/components/modal.js
+++ src/components/modal.js
@@ -14,7 +14,7 @@ export function Modal({ isOpen, onClose, caption }) {
       <div className="modal-content">
         {}
         <img
-          src="https://cdn.discordapp.com/attachments/928038965129392151/1159871616512163961/PXL_20210226_194643573.PORTRAIT2.jpg?ex=6532999e&is=6520249e&hm=537421b93a8fb2404bdc44b65a9d92a9901577a7cffaf3b7816b27185730c2a1&"
+          src="https://i.imgur.com/anM7JLU.jpeg"
           alt="moog"
         />
         {caption && <p className="modal-caption">{caption}</p>}
